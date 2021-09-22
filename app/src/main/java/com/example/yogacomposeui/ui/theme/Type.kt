3172927fd27066a2cb25636243bc6bb0933a96ec
Nilanjan <nilthedev@gmail.com>
2021-09-22 22:09:45 +0530

    added colors, text-styles and fonts

--- app/src/main/java/com/example/yogacomposeui/ui/theme/Type.kt
+++ app/src/main/java/com/example/yogacomposeui/ui/theme/Type.kt
@@ -2,17 +2,41 @@ package com.example.yogacomposeui.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.yogacomposeui.R
+
+val Poppins = FontFamily(
+    listOf(
+        Font(R.font.poppins_regular, FontWeight.Normal),
+        Font(R.font.poppins_bold, FontWeight.Bold),
+        Font(R.font.poppins_light, FontWeight.Light),
+        Font(R.font.poppins_semi_bold, FontWeight.SemiBold)
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 16.sp,
+    ),
+    h1 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = 26.sp,
+    ),
+
+    h2 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
     )
+
+
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
